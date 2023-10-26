4170c8ab6f335f89b368978d07b979d2cc31cacd
MyselfLeo <myselfleo@outlook.fr>
2023-10-27 00:01:26 +0200

    /

--- src/repl.rs
+++ src/repl.rs
@@ -1,13 +1,34 @@
 use std::io;
-use std::io::{Read, Write};
-use crossterm::{execute, queue, QueueableCommand};
+use std::io::Write;
+use crossterm::{execute, QueueableCommand};
 use crossterm::cursor::MoveTo;
 use crossterm::terminal;
+use crate::inductive::Formula;
 use crate::proof::Proof;
+use crate::repl::ReplError::CommandError;
+use crate::repl::ReplState::{Idle, Proving};
+use crate::rule::Rule;
+
 
 pub enum ReplState {
     Idle,
-    Proving(Proof)
+    Proving(Proof),
+    Quitting
+}
+
+impl PartialEq for ReplState {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (ReplState::Idle, ReplState::Idle) => true,
+            (ReplState::Proving(_), ReplState::Proving(_)) => true,
+            (ReplState::Quitting, ReplState::Quitting) => true,
+            _ => false
+        }
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        !self.eq(other)
+    }
 }
 
 
@@ -19,14 +40,18 @@ pub enum ReplCommand {
     Axiom,
     Qed,
 
-    Quit
+    Quit,
+    Help
 }
 
 
+#[derive(Debug)]
 pub enum ReplError {
     EmptyCommand,
     TooMuchArguments,
     UnknownCommand,
+    InvalidCommand,
+    CommandError(String),
     UnableToRead
 }
 
@@ -42,7 +67,7 @@ impl ReplCommand {
         };
 
         let cmd = match (cname, cparam) {
-            ("Proof", s) => ReplCommand::Proof(s.to_string()),
+            ("proof", s) => ReplCommand::Proof(s.to_string()),
             ("intro", s) => ReplCommand::Intro(s.to_string()),
 
             ("split", "") => ReplCommand::Split(None),
@@ -50,14 +75,14 @@ impl ReplCommand {
 
             ("elim", s) => ReplCommand::Elim(s.to_string()),
             ("axiom", "") => ReplCommand::Axiom,
+            ("qed", "") => ReplCommand::Qed,
 
-            ("Qed", "") => ReplCommand::Qed,
-
-            ("Quit", "") => ReplCommand::Quit,
+            ("quit", _) => ReplCommand::Quit,
+            ("help", "") => ReplCommand::Help,
 
             ("axiom", s) => return Err(ReplError::TooMuchArguments),
-            ("Qed", s) => return Err(ReplError::TooMuchArguments),
-            ("Quit", s) => return Err(ReplError::TooMuchArguments),
+            ("qed", s) => return Err(ReplError::TooMuchArguments),
+            ("help", s) => return Err(ReplError::TooMuchArguments),
             _ => return Err(ReplError::UnknownCommand)
         };
 
@@ -80,12 +105,11 @@ impl Repl {
         execute!(io::stdout(), terminal::EnterAlternateScreen)?;
 
         // Run Repl
-        let quit = false;
-        while !quit {
-            self.print_screen()?;
+        while self.state != ReplState::Quitting {
+            self.update()?;
 
             match self.get_command() {
-                Ok(_) => {}
+                Ok(c) => { self.execute(c).unwrap(); },
                 Err(_) => {}
             }
         }
@@ -94,20 +118,25 @@ impl Repl {
     }
 
 
-    fn print_screen(&mut self) -> io::Result<()> {
-        queue!(io::stdout(), MoveTo(0, 0), terminal::Clear(terminal::ClearType::FromCursorDown));
+    fn update(&mut self) -> io::Result<()> {
+        execute!(io::stdout(), MoveTo(0, 0), terminal::Clear(terminal::ClearType::FromCursorDown));
 
-        match self.state {
+        match &self.state {
             ReplState::Idle => {
                 println!("deducnat - v0.1.0");
-                println!("type 'help' for command information");
+                println!("type 'help' for command information, 'quit' to leave");
             }
-            ReplState::Proving(_) => {}
+
+            ReplState::Proving(p) => {
+                p.print();
+            }
+            ReplState::Quitting => {}
         }
 
         // Command prompt
-        let final_row = terminal::window_size()?.rows - 1;
-        queue!(io::stdout(), MoveTo(final_row, 0));
+        let final_row = terminal::window_size()?.rows;
+        execute!(io::stdout(), MoveTo(0, final_row));
+
         print!("> ");
 
         io::stdout().flush()
@@ -122,5 +151,60 @@ impl Repl {
         };
 
         ReplCommand::from(&txt)
+    }
+
+
+
+    fn execute(&mut self, command: ReplCommand) -> Result<(), ReplError> {
+        match command {
+            ReplCommand::Proof(p) => {
+                if self.state != Idle {Err(ReplError::InvalidCommand)}
+                else {
+                    let formula = match Formula::from_str(&p) {
+                        Ok(f) => f,
+                        Err(e) => return Err(ReplError::CommandError(e))
+                    };
+
+                    let proof = Proof::start(formula);
+                    self.state = Proving(proof);
+
+                    Ok(())
+                }
+            },
+            ReplCommand::Intro(s) => {
+                match self.state {
+                    Proving(ref mut p) => match p.apply(Rule::Intro(s)) {
+                        Ok(_) => Ok(()),
+                        Err(e) => Err(CommandError(e))
+                    },
+                    _ => Err(ReplError::InvalidCommand)
+                }
+            },
+            ReplCommand::Split(_) => unimplemented!(),
+            ReplCommand::Elim(s) => {
+                match self.state {
+                    Proving(ref mut p) => match p.apply(Rule::Elim(s)) {
+                        Ok(_) => Ok(()),
+                        Err(e) => Err(CommandError(e))
+                    },
+                    _ => Err(ReplError::InvalidCommand)
+                }
+            },
+            ReplCommand::Axiom => {
+                match self.state {
+                    Proving(ref mut p) => match p.apply(Rule::Axiom) {
+                        Ok(_) => Ok(()),
+                        Err(e) => Err(CommandError(e))
+                    },
+                    _ => Err(ReplError::InvalidCommand)
+                }
+            },
+            ReplCommand::Qed => unimplemented!(),
+            ReplCommand::Quit => {
+                self.state = ReplState::Quitting;
+                Ok(())
+            },
+            ReplCommand::Help => unimplemented!()
+        }
     }
 }
